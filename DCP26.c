9fcd2d467819daad638bc685ebea0d17a692624a
Brandon Fitzpatrick <49811439+brandonfitzy@users.noreply.github.com>
2019-08-08 15:11:30 -0400

    Update DCP26.c

--- DCP26.c
+++ DCP26.c
@@ -6,17 +6,20 @@
 #include <stdio.h>
 
 int main() {
-    int scheds [5][2] = {{30,75},{0,50},{60,150},{0,160},{170,180}};
+    int scheds [5][2] = {{30,75},{0,50},{60,150},{160,170},{0,200}};
     int rooms = 1;
-    int i; int j; int k; int l;
+    int next;
+    int i; int j;
     for (i=0;i<4;i++){
-        for (j=i+1;j<4;j++){
-            if (scheds[i][1]>scheds[j][0]){
-                rooms++;
-                printf("%d %d %d \n", i, j, rooms);
+        for (j=i+1;j<5;j++){
+            if (scheds[i][1]>scheds[j][0]  && scheds[i][1]<scheds[j][1]){
+                next=1;
+            } else {
+                next=0;
             }
         }
-        
+        rooms+=next;
+        next=0;
     }
     printf("You need %d", rooms);
 	return 0;
