3b498ebb3fb0487ae66badbd0c17db1b79810d1e
Brandon Fitzpatrick <49811439+brandonfitzy@users.noreply.github.com>
2019-08-08 16:13:34 -0400

    Update DCP26.c

--- DCP26.c
+++ DCP26.c
@@ -7,20 +7,21 @@
 
 int main() {
     int scheds [5][2] = {{30,75},{0,50},{60,150},{160,170},{0,200}};
-    int rooms = 1;
-    int next;
+    int rooms=1;
+    int next=0;
     int i; int j;
     for (i=0;i<4;i++){
         for (j=i+1;j<5;j++){
-            if (scheds[i][1]>scheds[j][0]  && scheds[i][1]<scheds[j][1]){
+            if (scheds[i][1]>scheds[j][0]  && scheds[i][1]<scheds[j][1] && scheds[i][0]>scheds[j][0]  && scheds[i][0]<scheds[j][1]){
                 next=1;
-            } else {
+                printf("%d \n",i);
+            }else{
                 next=0;
             }
         }
-        rooms+=next;
-        next=0;
+        rooms=rooms+next;
     }
-    printf("You need %d", rooms);
+    printf("%d", rooms);
+
 	return 0;
 }
