966a923bfd5be74af31d1fa03f848d8423a1069f
Brandon Fitzpatrick <49811439+brandonfitzy@users.noreply.github.com>
2019-08-12 14:06:41 -0400

    Add files via upload

---
+++ passwordcheck.c
@@ -0,0 +1,65 @@
+/*Password validator*/
+
+#include <stdio.h>
+#include <string.h>
+
+char numbers[]={'0','1','2','3','4','5','6','7','8','9'};
+char uppercase[]={"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+char lowercase[]={"abcdefghijklmnopqrstuvwxyz"};
+char specialchars[]={'!','@','#','$','%','^','&','*','(',')','?'};
+
+void passwordvalidator(int *a, int b);
+
+int main(){
+	char password[]={"Password1!"};
+    int * passwordpointer = & password;
+	passwordvalidator(passwordpointer, strlen(password));
+	return 0;
+}
+
+void passwordvalidator(int *a, int b){
+	int l;
+	char array[b];
+	for (l=0;l<b;l++){
+		array[l]=a[l];
+	}
+	int num=0; int up=0; int low=0; int spec=0;
+	int i; int j;
+	for (i=0;i<strlen(array);i++){
+		for (j=0;j<=strlen(numbers);j++){
+			if (array[i]==numbers[j]){
+				num=1;
+			} 
+		}
+		for (j=0;j<=strlen(uppercase);j++){
+			if (array[i]==uppercase[j]){
+				up=1;
+			} 
+		}
+		for (j=0;j<=strlen(lowercase);j++){
+			if (array[i]==lowercase[j]){
+				low=1;
+			} 
+		}
+		for (j=0;j<=strlen(specialchars);j++){
+			if (array[i]==specialchars[j]){
+				spec=1;
+			} 
+		}
+	}
+	if (num==0){
+		printf("You need atleast one number. \n");
+	}
+	if (up==0){
+		printf("You need atleast one uppercase letter. \n");
+	}
+	if (low==0){
+		printf("You need atleast one lowercase letter. \n");
+	}
+	if (spec==0){
+		printf("You need atleast one special character. \n");
+	}
+	if (num==1 && up==1 && low==1 && spec==1) {
+		printf("Password meets requirements. \n");
+	}
+}
