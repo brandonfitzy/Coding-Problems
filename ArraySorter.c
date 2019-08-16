a177426a163a612ca7c76ee38914ee9767b8ae48
Brandon Fitzpatrick <49811439+brandonfitzy@users.noreply.github.com>
2019-08-16 13:20:52 -0400

    Update ArraySorter.c

--- ArraySorter.c
+++ ArraySorter.c
@@ -2,23 +2,31 @@
 
 #include <stdio.h>
 
-int main(){
-	int arr[] = {23,63,41,72,73,10,99};
-	int i; int j; int value;
-	for (i=0; i<=5; i++){
-		for (j=i+1;j<=6; j++){
-			if (arr[i]<arr[j]){
+#define SIZE_OF_ARRAY   (8)
+#define MIN(x,y)        (x<y)
+
+int main()
+{
+	int arr[SIZE_OF_ARRAY] = {23,63,41,72,73,10,99,100};
+	int i; int j;
+	for (i=0; i<=(SIZE_OF_ARRAY)-2; i++)
+    {
+		for (j=i+1;j<=(SIZE_OF_ARRAY-1); j++)
+		{
+			if (MIN(arr[i],arr[j]))
+            {
+                int value;
 				value=arr[i];
 				arr[i]=arr[j];
 				arr[j]=value;
 			}
-				
+
 		}
-		
+
 	}
-	int k;
-	for (k=0;k<=6;k++){
-		printf("%d ", arr[k]);
+	for (i=0;i<=(SIZE_OF_ARRAY-1);i++)
+    {
+		printf("%d ", arr[i]);
 	}
 	return 0;
 }
