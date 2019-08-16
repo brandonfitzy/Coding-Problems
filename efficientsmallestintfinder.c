5393bb37ae89df4088335aa068fe431e65f61d50
Brandon Fitzpatrick <49811439+brandonfitzy@users.noreply.github.com>
2019-08-16 16:02:45 -0400

    Update efficientsmallestintfinder.c

--- efficientsmallestintfinder.c
+++ efficientsmallestintfinder.c
@@ -1,77 +1,19 @@
-/*Point in a Shape*/
-
-/*Determines if a given point lies in a given polygon*/
-
-/*1=true and 0=false*/
-
 #include <stdio.h>
+#include <stdlib.h>
 
-//struct for polygon
-struct polygon{
-	int points[3][2];
-};
+#define ARRAY_SIZE 9
 
-//struct for the point
-struct point{
-	int coord[2];
-};
-
-//global structs
-struct polygon shape;
-struct point p;
-
-//function to determine if horizontal line at point p interesects with any sides of the polygon
-int dointersect(int py, int sy1, int sy2);
-
-int main() {
-	//creating the specific point
-	p.coord[1]=0;
-	p.coord[2]=1;
-	//creating the specific polygon
-	shape.points[1][1]=0;
-	shape.points[1][2]=0;
-	shape.points[2][1]=3;
-	shape.points[2][2]=0;
-	shape.points[3][1]=3;
-	shape.points[3][2]=4;
-	
-	//counting the intersectings
-	int i;
-	int intersects=0;
-	for (i=1;i<=3;i++){
-		if (i==3){
-			intersects+=dointersect(p.coord[2], shape.points[i][2], shape.points[1][2]);
-			
-		} else {
-			intersects+=dointersect(p.coord[2], shape.points[i][2], shape.points[i+1][2]);
-		}
-	}
-	//if there is an even amount of intersects the point is outside
-	if (intersects%2==0){
-		printf("The point p lies outside of the polygon");
-	//else the number is odd and the point in inside the polygon
-	} else {
-		printf("The point p lies inside of the polygon");
-	}
-	
-	return 0;
-}
-
-
-//function to check intersection (not including points on the line)
-int dointersect(int py, int sy1, int sy2){
-	int b;
-	if (sy1<sy2){
-		int y1=sy1;
-		int y2=sy2;
-	} else {
-		int y1=sy2;
-		int y2=sy1;
-	}
-	if (py>y1 && py<y2){
-		b=1;
-	} else {
-		b=0;
-	}
-	return b;
+int main()
+{
+    int array[]={23,68,45,43,11,99,78,52,61};
+    int largest=0;
+    int i;
+    for (i=0;i<ARRAY_SIZE;i++)
+    {
+        if (array[i]>largest){
+            largest=array[i];
+        }
+    }
+    printf("%d", largest);
+    return 0;
 }
