a4c6d0b381e3d27c13a2a465603c25633428fb96
Brandon Fitzpatrick <49811439+brandonfitzy@users.noreply.github.com>
2019-08-16 16:04:13 -0400

    Update efficientsmallestintfinder.c

--- efficientsmallestintfinder.c
+++ efficientsmallestintfinder.c
@@ -5,15 +5,16 @@
 
 int main()
 {
-    int array[]={23,68,45,43,11,99,78,52,61};
-    int largest=0;
+    int array[] = { 23, 68, 45, 43, 11, 99, 78, 52, 61 };
+    int largest = 0;
     int i;
-    for (i=0;i<ARRAY_SIZE;i++)
+    for ( i=0 ; i < ARRAY_SIZE ; i++ )
     {
-        if (array[i]>largest){
-            largest=array[i];
+        if ( array[i] > largest )
+        {
+            largest = array[i];
         }
     }
-    printf("%d", largest);
+    printf( "%d" , largest );
     return 0;
 }
